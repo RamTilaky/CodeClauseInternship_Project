5be65ef6cbfc5c1288fcd196ed17c2331c41a2c1
RamTilaky <132215854+RamTilaky@users.noreply.github.com>
2024-03-31 23:15:41 +0530

    Delete Atm directory

--- Atm/src/mypackage/AtmOperationinter.java
+++
@@ -1,8 +0,0 @@
-package mypackage;
-
-public interface AtmOperationinter {
-	public void viewBalance();
-    public void withdrawAmount(double withdrawAmount);
-    public void depositAmount(double depositAmount );
-    public  void viewMiniStatement();
-}
